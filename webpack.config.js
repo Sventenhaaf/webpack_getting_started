4f1dad69197a69104400f7816915815f03c4a1d1
Sven ten Haaf <sventenhaaf@hotmail.com>
2015-12-01 13:21:07 +0100

    up and running

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  context: __dirname,
+  entry: "./hello_world.jsx",
+  output: {
+    path: "./",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  }
+};
